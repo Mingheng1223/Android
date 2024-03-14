803afa52f7d3a774958c36804853fd700bac9d67
Mingheng1223 <a109221080@mail.shu.edu.tw>
2024-03-14 17:27:28 +0800

    update

--- TempTransfer/app/src/main/java/com/example/temptransfer/MainActivity.java
+++ TempTransfer/app/src/main/java/com/example/temptransfer/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.temptransfer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void button_click(View view){
+        EditText edittemp=(EditText) findViewById(R.id.editTemp);
+        TextView textshow=(TextView) findViewById(R.id.textshow);
+
+        double DegreeC=Double.parseDouble(edittemp.getText().toString());
+        double DegreeF=(9.0*DegreeC)/5.0+32.0;
+        textshow.setText("華氏溫度:"+String.format("%.2f",DegreeF));
     }
 }
