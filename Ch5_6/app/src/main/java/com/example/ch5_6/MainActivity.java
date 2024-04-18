7858bdb305000c54d4673fb6f55e2e3053f16e0b
Mingheng1223 <a109221080@mail.shu.edu.tw>
2024-04-18 16:50:38 +0800

    1130418

--- Ch5_6/app/src/main/java/com/example/ch5_6/MainActivity.java
+++ Ch5_6/app/src/main/java/com/example/ch5_6/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
-        implements View.OnClickListener {
+         {
     private float size = 20;
     private TextView output;
     @Override
@@ -19,17 +19,29 @@ public class MainActivity extends AppCompatActivity
         output.setTextSize(size);
         Button btn = (Button) findViewById(R.id.button);
         Button btn2 = (Button) findViewById(R.id.button2);
-        btn.setOnClickListener(this);
-        btn2.setOnClickListener(this);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                size++;
+                output.setTextSize(size);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                size--;
+                output.setTextSize(size);
+            }
+        });
     }
-    @Override
-    public void onClick(View view) {
-        if (view.getId() == R.id.button) {
-            size++;
-        }
-        else {
-            size--;
-        }
-        output.setTextSize(size);
-    }
+//    @Override
+//    public void onClick(View view) {
+//        if (view.getId() == R.id.button) {
+//            size++;
+//        }
+//        else {
+//            size--;
+//        }
+//        output.setTextSize(size);
+//    }
 }
