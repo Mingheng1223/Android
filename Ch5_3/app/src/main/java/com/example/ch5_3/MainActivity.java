9aae4f738d7766249d93d85f2e6c7731034fdb48
Mingheng1223 <a109221080@mail.shu.edu.tw>
2024-04-18 15:52:20 +0800

    0418
    update

--- Ch5_3/app/src/main/java/com/example/ch5_3/MainActivity.java
+++ Ch5_3/app/src/main/java/com/example/ch5_3/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.ch5_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +19,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txvOutput=(TextView) findViewById(R.id.lblOutput);
-        txvOutput.setTextSize(original_size);
+        txvOutput.setTextSize(TypedValue.COMPLEX_UNIT_PX,original_size);
         Button btnEnlarge =(Button)findViewById(R.id.button);
         btnEnlarge.setOnClickListener(this);
         btnEnlarge.setOnLongClickListener(this);
@@ -26,11 +28,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v){
         float size=txvOutput.getTextSize();
-        txvOutput.setTextSize(size+1);
+        txvOutput.setTextSize(px2sp(this,size)+5);
     }
     @Override
     public boolean onLongClick(View v){
-        txvOutput.setTextSize(original_size);
+        txvOutput.setTextSize(TypedValue.COMPLEX_UNIT_PX,original_size);
         return false;
     }
+
+    public  static int px2sp(Context context,float pxValue){
+        final float fontScale=context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue/fontScale+0.5f);
+    }
+
 }
